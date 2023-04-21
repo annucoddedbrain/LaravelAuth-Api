60c9efce70dfc7274205cf07390277324ab16fcf
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-21 15:27:36 +0530

    registration process is done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -14,11 +15,25 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $validator = Validator::make($request->all, [
+        $validator = Validator::make($request->all(),[
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed|min:6',
         ]);
+
+        if($validator -> fails()){
+            return response()->json($validator->errors()->toJson(),400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password'=> bcrypt($request->password)]
+        ));
+
+        return response() -> json([
+            "message" => "User Registered Successfully",
+            'user' => $user
+        ],201);
     }
 
     public function login(Request $request){
