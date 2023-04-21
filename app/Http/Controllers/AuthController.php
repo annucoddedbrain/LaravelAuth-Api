ebb00eeab0966f19b7b6a7b02ca21be08f73ff21
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-21 15:09:54 +0530

    create register validation in Auth Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -13,7 +14,11 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-
+        $validator = Validator::make($request->all, [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
     }
 
     public function login(Request $request){
