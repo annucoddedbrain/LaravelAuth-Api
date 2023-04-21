5432bb872254e384044302c2bd05cc94b03fe959
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-21 14:34:55 +0530

    create authentication setup

--- config/app.php
+++ config/app.php
@@ -194,6 +194,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
 
     ],
 
@@ -210,6 +212,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'JWtAuth'=>Tymon\JWTAuth\Facades\JWtAuth::class,
+        'JWtAuthFactory'=>Tymon\JWTAuth\Facades\JWtAuthFactory::class,
+
     ])->toArray(),
 
 ];
